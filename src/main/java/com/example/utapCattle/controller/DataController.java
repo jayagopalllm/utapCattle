0d8eb0203835bb1c78ff4958411f31109bf4c93a
bharath <bharath2103@gmail.com>
2024-08-16 15:59:26 +0100

    edited datacontroller

--- src/main/java/com/example/utapCattle/controller/DataController.java
+++ src/main/java/com/example/utapCattle/controller/DataController.java
@@ -2,18 +2,20 @@ package com.example.utapCattle.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.utapCattle.model.dto.AllDataDto;
 import com.example.utapCattle.service.DataService;
 
 @RestController
+@RequestMapping("/data")
 public class DataController {
     
     @Autowired
     private DataService dataService;
 
-    @GetMapping("/data")
+    @GetMapping()
     public AllDataDto getAllData() {
         return dataService.getAllData();
     }
