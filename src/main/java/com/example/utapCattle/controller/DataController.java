27285f67fc12000cee9401f83e6320d5ed72116d
Muralikkannnan <roshnamurali2013@gmail.com>
2024-08-16 09:58:57 +0100

    cors changes

--- src/main/java/com/example/utapCattle/controller/DataController.java
+++ src/main/java/com/example/utapCattle/controller/DataController.java
@@ -2,18 +2,20 @@ package com.example.utapCattle.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.utapCattle.model.dto.AllDataDto;
 import com.example.utapCattle.service.DataService;
 
 @RestController
+@CrossOrigin (origins = {"http://localhost:4200", "http://35.178.210.158"})
 public class DataController {
     
     @Autowired
     private DataService dataService;
 
-    @GetMapping("/data")
+    @GetMapping("/data")   
     public AllDataDto getAllData() {
         return dataService.getAllData();
     }
