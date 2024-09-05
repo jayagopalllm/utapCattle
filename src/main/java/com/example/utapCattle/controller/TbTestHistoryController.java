c526edecdcf7b06942c784666897406e6eba54a5
Balaji N <balajin@Balajis-MacBook-Air.local>
2024-09-05 23:38:23 +0530

    TB Test API Change

--- src/main/java/com/example/utapCattle/controller/TbTestHistoryController.java
+++ src/main/java/com/example/utapCattle/controller/TbTestHistoryController.java
@@ -8,19 +8,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.utapCattle.model.entity.TbTestHistory;
+import com.example.utapCattle.model.entity.TreatmentHistoryMetadata;
 import com.example.utapCattle.service.TbTestHistoryService;
+import com.example.utapCattle.service.TreatmentHistoryService;
 
 @RestController
 @RequestMapping("/tbtest")
-public class TbTestHistoryController {
+public class TbTestHistoryController extends BaseController {
 
 	@Autowired
 	private TbTestHistoryService tbTestHistoryService;
 
+	@Autowired
+	private TreatmentHistoryService treatmentHistoryService;
+
 	@PostMapping("/save")
-	public ResponseEntity<?> saveTbTestHistory(@RequestBody TbTestHistory tbTestHistory) {
+	public ResponseEntity<?> saveTbTestHistory(@RequestBody final TreatmentHistoryMetadata treatmentHistoryMetadata) {
 		try {
+			logger.info("Incoming request: Saving tb test information");
+			// add process id
+			treatmentHistoryMetadata.setProcessId(3L);
+			treatmentHistoryMetadata.setCattleId(treatmentHistoryMetadata.getCattleId());
+
+			final TbTestHistory tbTestHistory = treatmentHistoryMetadata.getTbTestHistory();
+
 			final TbTestHistory savedTbTestHistory = tbTestHistoryService.saveTbTestHistory(tbTestHistory);
+
+			treatmentHistoryService.saveTreatmentHistory(treatmentHistoryMetadata);
+
 			return ResponseEntity.ok(savedTbTestHistory);
 		} catch (final Exception e) {
 			return ResponseEntity.badRequest().body(e.getMessage());
