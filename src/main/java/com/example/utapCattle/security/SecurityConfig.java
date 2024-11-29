0588e8bdc0d6a083d6d77f07cbbaae1a9003137b
Mohsin Naeem <mohsin.naeem@sigma-emea.com>
2024-11-29 19:35:48 +0500

    commenting spring security

--- src/main/java/com/example/utapCattle/security/SecurityConfig.java
+++ src/main/java/com/example/utapCattle/security/SecurityConfig.java
@@ -1,25 +1,25 @@
-package com.example.utapCattle.security;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-//@EnableWebSecurity
-public class SecurityConfig {
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        // Disable CSRF as you're building a stateless API (if needed)
-//        http.csrf(AbstractHttpConfigurer::disable)
-//                // Allow all requests (no authentication required)
-//                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/**").permitAll() // Permit all requests
-//                        .anyRequest().permitAll())  // Ensure that any other requests are also permitted
-//                // Disable session management (stateless API)
-//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//package com.example.utapCattle.security;
 //
-//        return http.build();
-//    }
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-}
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+////@EnableWebSecurity
+//public class SecurityConfig {
+////    @Bean
+////    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+////        // Disable CSRF as you're building a stateless API (if needed)
+////        http.csrf(AbstractHttpConfigurer::disable)
+////                // Allow all requests (no authentication required)
+////                .authorizeHttpRequests(auth -> auth
+////                        .requestMatchers("/**").permitAll() // Permit all requests
+////                        .anyRequest().permitAll())  // Ensure that any other requests are also permitted
+////                // Disable session management (stateless API)
+////                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+////
+////        return http.build();
+////    }
+////    @Bean
+////    public PasswordEncoder passwordEncoder() {
+////        return new BCryptPasswordEncoder();
+////    }
+//}
