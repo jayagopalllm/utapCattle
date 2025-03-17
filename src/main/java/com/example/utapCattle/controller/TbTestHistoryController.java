d7e93e2b25772377388de48f33888982064368d5
mohsin <mohsin.naeem@sigma-emea.com>
2025-03-17 17:40:01 +0500

    tb test changes

--- src/main/java/com/example/utapCattle/controller/TbTestHistoryController.java
+++ src/main/java/com/example/utapCattle/controller/TbTestHistoryController.java
@@ -4,19 +4,18 @@ import com.example.utapCattle.model.entity.TbTestHistory;
 import com.example.utapCattle.model.entity.TreatmentHistoryMetadata;
 import com.example.utapCattle.service.TbTestHistoryService;
 import com.example.utapCattle.service.TreatmentHistoryService;
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.SecureRandom;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/tbtest")
-@CrossOrigin(origins = { "http://localhost:4200", "http://35.178.210.158" })
+//@CrossOrigin(origins = { "http://localhost:4200", "http://35.178.210.158" })
 public class TbTestHistoryController extends BaseController {
 
 	private final TbTestHistoryService tbTestHistoryService;
@@ -44,4 +43,27 @@ public class TbTestHistoryController extends BaseController {
         	return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
 		}
 	}
+
+	@GetMapping("/{earTagOrEId}")
+	public ResponseEntity<Map<String, Object>> getCattleDetailsAndAverageConditionScore(
+			@PathVariable("earTagOrEId") final String earTagOrEId) {
+		if (StringUtils.isEmpty(earTagOrEId)) {
+			logger.warn("Invalid earTagOrEId provided");
+			return ResponseEntity.badRequest().build();
+		}
+		try {
+			final Map<String, Object> outputMap = tbTestHistoryService
+					.getCattleDetailsAndTBTest(earTagOrEId);
+			if (MapUtils.isEmpty(outputMap)) {
+				logger.warn("No cattle info found for earTagOrEId: {}", earTagOrEId);
+				return ResponseEntity.noContent().build();
+			}
+			logger.info("Retrieved Cattle info and avg condition score");
+			return ResponseEntity.ok(outputMap);
+		} catch (final Exception e) {
+			logger.error("Exception occurred: Unable to retrieve cattle info for earTagOrEId: {}", earTagOrEId, e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
+	}
+
 }
