77e9ebeda93c74493e29a3b017118748ecb42bad
Dibin Peter <peter.dibin@gmail.com>
2025-05-01 18:15:35 +0100

    Added overall dlwg to "Cattles Weighed Today" grid Weight Page

--- src/main/java/com/example/utapCattle/model/dto/WeightHistDto.java
+++ src/main/java/com/example/utapCattle/model/dto/WeightHistDto.java
@@ -4,8 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
@@ -15,4 +13,5 @@ public class WeightHistDto {
     private Long cattleId;
     private String weightDateTime;
     private Double weight;
+    private Double dlwgFarm;
 }
